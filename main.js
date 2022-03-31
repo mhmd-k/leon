d2c2ddd021f4e4303ace9f16a4a188e5d7bb1536
mhmd-k <91424162+mhmd-k@users.noreply.github.com>
2022-03-31 03:03:08 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 // navbar
 let headLinks = document.getElementById("links");
 let headIcon = document.getElementById("icon");
-headLinks.onclick = function () { 
-    if (headIcon.style.display === "none") {
+headLinks.onclick = function () {
+    if (headIcon.style.display === "none" || headIcon.style.display === "") {
         headIcon.style.display = "block";
-    } else { 
+    } else {
         headIcon.style.display = "none";
     }
-}
+};
 // scroll to top button
 let btn = document.querySelector(".scroll-to-top");
 window.onscroll = function () {
