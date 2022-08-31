3c7c99bc9a40767f889201df581801366e0ca078
Mohammed Alkayyali <mhmdk425@gmail.com>
2022-08-31 08:51:55 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,33 +1,74 @@
 // navbar
 let navBtn = document.querySelector(".icon");
 let links = document.querySelector("#icon");
-navBtn.onclick = function (e) {
-    e.stopPropagation();
-    if (!links.classList.contains("open")) {
-        links.classList.add("open");
-    } else {
-        links.classList.remove("open");
-    }
-};
-document.onclick = function () {
+navBtn.addEventListener("click", (e) => {
+  e.stopPropagation();
+  if (!links.classList.contains("open")) {
+    links.classList.add("open");
+  } else {
     links.classList.remove("open");
-};
-links.onclick = function (e) {
-    e.stopPropagation();
-};
+  }
+});
+document.addEventListener("click", function () {
+  links.classList.remove("open");
+});
+
 // scroll to top button
 let btn = document.querySelector(".scroll-to-top");
-window.onscroll = function () {
-    if (window.scrollY >= 500) {
-        btn.style.display = "block";
-    } else {
-        btn.style.display = "none";
-    }
-};
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 500) {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
+  }
+});
 btn.onclick = function () {
-    window.scrollTo({
-        left: 0,
-        top: 0,
-        behavior: "smooth",
-    });
+  window.scrollTo({
+    left: 0,
+    top: 0,
+    behavior: "smooth",
+  });
 };
+
+// elements apper on scroll
+window.addEventListener("scroll", () => {
+  apperingInScroll(
+    document.querySelectorAll(".features .container .feat "),
+    "visible",
+    350
+  );
+  apperingInScroll(
+    document.querySelectorAll(".container .father-icons .col"),
+    "visible",
+    350
+  );
+  apperingInScroll(
+    document.querySelectorAll("h2.special-heading"),
+    "onscroll",
+    350
+  );
+  apperingInScroll(document.querySelectorAll(".col-2"), "visible", 350);
+  apperingInScroll(document.querySelectorAll(".about-image"), "visible", 350);
+  apperingInScroll(document.querySelectorAll(".about-text p"), "visible", 350);
+  apperingInScroll(document.querySelectorAll(".mail"), "visible", 400);
+  apperingInScroll(
+    document.querySelectorAll("h2.special-heading:last-of-type"),
+    "onscroll",
+    400
+  );
+  if (screen.width < 767) {
+    apperingInScroll(document.querySelectorAll(".mail"), "visible", 700);
+    apperingInScroll(
+      document.querySelectorAll("h2.special-heading:last-of-type"),
+      "onscroll",
+      600
+    );
+  }
+});
+
+function apperingInScroll(arr, c, num) {
+  arr.forEach((ele) => {
+    if (window.scrollY >= ele.offsetTop - num) ele.classList.add(c);
+    else ele.classList.remove(c);
+  });
+}
